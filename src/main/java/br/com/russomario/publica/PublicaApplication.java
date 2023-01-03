2ad75e7ab3e0895d3adfec24a9e146e03bf8d726
Mário Russo <mario.russo93@hotmail.com>
2023-01-02 22:42:24 -0300

    início do projeto

---
+++ src/main/java/br/com/russomario/publica/PublicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.russomario.publica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/russomario/publica/PublicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.russomario.publica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
