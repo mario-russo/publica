9674d41197311e985d4d4d52b0e8671da2c737d1
Mário Russo <mario.russo93@hotmail.com>
2023-01-05 22:45:40 -0300

    Test (usuario) : teste para serviço do usuário

--- src/test/java/br/com/russomario/publica/service/UsuarioServiceImplTest.java
+++ src/test/java/br/com/russomario/publica/service/UsuarioServiceImplTest.java
@@ -1,11 +1,17 @@
 package br.com.russomario.publica.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
@@ -18,36 +24,97 @@ import br.com.russomario.publica.repository.UsuarioRepository;
 
 @ExtendWith(MockitoExtension.class)
 public class UsuarioServiceImplTest {
+
     @Mock
     private UsuarioRepository repository;
+
     @InjectMocks
     private UsuarioServiceImpl usuarioServiceImpl;
-    
+
     UsuarioRequestDTO usuarioRequestDTO;
     UsuarioRespostaDTO usuarioRespostaDTO;
-    Optional<Usuario> usuario;
-    
-    
+    Usuario usuario;
+
     @BeforeEach
-    void inicio(){
-        usuario.    }
-    @Test
-    void testBuscaId() {
-        when(repository.findById(anyLong())).thenReturn(Optional<usuario> );
+    void inicio() {
+        MockitoAnnotations.openMocks(this);
+        usuario = new Usuario("mario", "mario@email");
+        usuarioServiceImpl = new UsuarioServiceImpl(repository);
+
+        usuarioRequestDTO = new UsuarioRequestDTO(usuario.getNome(), usuario.getEmail());
+        usuarioRespostaDTO = new UsuarioRespostaDTO(usuario.getId(), usuario.getNome(), usuario.getEmail(),
+                usuario.getPublicacao());
     }
 
+    @DisplayName("deve retorna o um usuário pelo id")
+    @Test
+    void testBuscaId() throws Exception {
+
+        Optional<Usuario> usuarioOptional;
+        usuarioOptional = Optional.of(usuario);
+
+        when(repository.findById((long) 1)).thenReturn(usuarioOptional);
+        var reposta = usuarioServiceImpl.BuscaId((long) 1);
+
+        assertEquals(UsuarioRespostaDTO.class, reposta.getClass());
+        assertEquals(usuario.getEmail(), reposta.getEmail());
+        assertEquals(usuario.getNome(), reposta.getNome());
+
+    }
+
+    @DisplayName("Deve lista os Usuários ")
     @Test
     void testListaTodos() {
 
+        List<UsuarioRespostaDTO> usuarioRespostaDTOs;
+
+        when(repository.findAll()).thenReturn(List.of(usuario));
+
+        usuarioRespostaDTOs = usuarioServiceImpl.listaTodos();
+
+        assertEquals(UsuarioRespostaDTO.class, usuarioRespostaDTOs.get(0).getClass());
+        assertEquals(usuario.getEmail(), usuarioRespostaDTOs.get(0).getEmail());
+        assertEquals(usuario.getNome(), usuarioRespostaDTOs.get(0).getNome());
+
     }
 
+    @DisplayName("Deve salvar o Usuario com sucesso ")
     @Test
-    void testSalvar() {
+    void testSalvar() throws Exception {
+
+        when(repository.save(any())).thenReturn(usuario);
+
+        usuarioRespostaDTO = usuarioServiceImpl.salvar(usuarioRequestDTO);
+
+        
+        verify(repository).save(ArgumentMatchers.any());
+        
+        assertEquals(UsuarioRespostaDTO.class, usuarioRespostaDTO.getClass());
+        assertEquals("mario", usuarioRespostaDTO.getNome());
+        assertEquals("mario@email", usuarioRespostaDTO.getEmail());
+        
 
     }
 
+    @DisplayName("Deve retorna uma mensagemde erro por passar o objeto null")
+    @Test
+    void testExceptionAoSalvar() throws Exception{
+
+        when(repository.save(any())).thenReturn(null);
+
+        assertThrowsExactly(Exception.class,()-> usuarioServiceImpl.salvar(usuarioRequestDTO), "Erro o Salva Usuário");
+    }
+
+    @DisplayName("Deve retorna uma Classe de Usuário")
     @Test
     void testUsuarioEntity() {
+        Optional<Usuario> usuarioOptional;
+        usuarioOptional = Optional.of(usuario);
 
+        when(repository.findById(anyLong())).thenReturn(usuarioOptional);
+
+        var reposta = usuarioServiceImpl.usuarioEntity(anyLong());
+
+        assertEquals(Usuario.class, reposta.getClass());
     }
 }
