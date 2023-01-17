516f22925a3fc361f904e835977f5f6140c469bc
Mário Russo <mario.russo93@hotmail.com>
2023-01-16 22:48:58 -0300

    fix ( Bug ) : removendo anotação do SpringBoot

--- src/test/java/br/com/russomario/publica/PublicaApplicationTests.java
+++ src/test/java/br/com/russomario/publica/PublicaApplicationTests.java
@@ -1,10 +1,8 @@
 package br.com.russomario.publica;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
 @ImportResource({"classpath*:application-context.xml"})
 class PublicaApplicationTests {
 
