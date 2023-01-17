59e9650a069dd704317b8e82c9f75ac601a25e8b
Mário Russo <mario.russo93@hotmail.com>
2023-01-16 22:43:56 -0300

    Test (SpringBootApplication) : Adicionando o ImportResource para teste

--- src/test/java/br/com/russomario/publica/PublicaApplicationTests.java
+++ src/test/java/br/com/russomario/publica/PublicaApplicationTests.java
@@ -1,9 +1,11 @@
 package br.com.russomario.publica;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
-@SpringBootTest
+@SpringBootApplication
+@ImportResource({"classpath*:application-context.xml"})
 class PublicaApplicationTests {
 
 	@Test
