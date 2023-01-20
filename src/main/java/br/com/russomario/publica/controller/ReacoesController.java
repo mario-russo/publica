2a77779ce7e19d5fe8f8cb683b52c0dc25837816
Mário Russo <mario.russo93@hotmail.com>
2023-01-20 00:10:39 -0300

    feat (controller reacões ) : post e get para reacões

--- src/main/java/br/com/russomario/publica/controller/ReacoesController.java
+++ src/main/java/br/com/russomario/publica/controller/ReacoesController.java
@@ -1,5 +1,10 @@
 package br.com.russomario.publica.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,10 +25,15 @@ public class ReacoesController {
     }
 
     @PostMapping
-    public ReacoesRepostaDto salva (@RequestBody ReacoesRequestDTO reacoes){
+    public ResponseEntity<ReacoesRepostaDto> salva (@RequestBody ReacoesRequestDTO reacoes){
         ReacoesRepostaDto reacoeSalva = serviceImpler.salvar(reacoes);
-        return reacoeSalva;
+        return ResponseEntity.status(HttpStatus.CREATED).body(reacoeSalva);
 
     }
+     @GetMapping
+     public ResponseEntity<List<ReacoesRepostaDto>> listaTodos(){
+        List<ReacoesRepostaDto> reacoes = serviceImpler.listaTodos();
+        return ResponseEntity.ok().body(reacoes);
+     }
     
 }
